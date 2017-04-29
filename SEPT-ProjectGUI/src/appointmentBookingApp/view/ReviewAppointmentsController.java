0f9a8a09bd2728c4aa9145fd5a639f88d64674a2
rmit-s3448344-Aydan-Schwartz <s3448344@student.rmit.edu.au>
2017-04-29 15:59:37 +1000

    Customer Hompage added
    - created customer hompage
    - homepage holds reference to current customer allowing for easier DB
    searches relating to customer.
    
    - added QoL method in util class, method will capitilse string.

---
+++ SEPT-ProjectGUI/src/appointmentBookingApp/view/ReviewAppointmentsController.java
@@ -0,0 +1,16 @@
+package appointmentBookingApp.view;
+
+
+import appointmentBookingApp.MainApp;
+import javafx.stage.Stage;
+
+public class ReviewAppointmentsController {
+    public void setDialogStage(Stage dialogStage) {
+    }
+
+    public void setMainApp(MainApp mainApp) {
+    }
+
+    public void ini() {
+    }
+}
